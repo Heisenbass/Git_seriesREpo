69c7ac5f4874a58d010b92b9f242ea93c08a77f7
Kutlwano <kutlwanoranaka21@gmail.com>
2025-12-09 22:19:26 +0200

    src file updated

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 5001;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
